6891481af109c4e560d635ffabd14f40cdc0e515
Zahid Bhatti <Zahidbhatti074@gmail.com>
2024-09-11 18:43:23 +0000

    Add models for orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, default='Pending')
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
+
+class OrderItems(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE) 
+    quantity = models.IntegerField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name}"
+
+    def get_total_price(self):
+        return self.quantity * self.price
